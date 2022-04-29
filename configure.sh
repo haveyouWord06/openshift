d7bfc2681a0a5272cdbcd02402866319c9b9c7df
haveyouWord06 <100894319+haveyouWord06@users.noreply.github.com>
2022-04-29 06:34:17 +0000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,7 +1,37 @@
 #!/bin/sh
 # V2Ray new configuration
+UUID=3bf7d881-26ce-4488-96ad-e3c9040fe400
+WSPATH=/vless
+PORT=80
+
 cat <<-EOF > /etc/v2ray/config.json
-$CONFIG_JSON
+{
+  "inbounds": [
+    {
+      "port": ${PORT},
+      "protocol": "vless",
+      "settings": {
+        "clients": [
+          {
+            "id": "${UUID}"
+          }
+        ],
+        "decryption": "none"
+      },
+      "streamSettings": {
+        "network": "ws",
+        "wsSettings": {
+          "path": "${WSPATH}"
+        }
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}
 EOF
 # Run V2Ray
 /usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
